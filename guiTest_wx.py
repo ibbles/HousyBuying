1f1d07501dd0839940f5c67b218b20e74aad9827
Martin Nilsson <ibbles@hotmail.com>
2014-03-11 21:50:08 +0100

    Moved plot data storage into the RateFrame class itself.

--- guiTest_wx.py
+++ guiTest_wx.py
@@ -17,27 +17,19 @@ from datetime import date
 from datetime import datetime
 
 
-interestAtDate_data = [
-  (date(2014, 1, 1), 3.0),
-  (date(2014, 2, 1), 2.0),
-  (date(2014, 3, 1), 3.0),
-  (date(2014, 4, 1), 4.0),
-  (date(2014, 5, 1), 3.0),
-  (date(2015, 3, 1), 4.0),
-  (date(2015, 8, 1), 5.0),
-  (date(2016, 2, 1), 6.0),
-  (date(2016, 5, 1), 7.0)
-]
-plotData = ([], [])
-for datePoint,rate in interestAtDate_data:
-  plotData[0].append(datePoint)
-  plotData[1].append(rate)
+
 
 class RateFrame(wx.Frame):
-  def __init__(self):
+  def __init__(self, interestRates):
     self.dragging = False
     self.draggedIndex = -1
     self.annotation = None
+
+    self.plotData = ([], [])
+    for datePoint,rate in interestAtDate_data:
+      self.plotData[0].append(datePoint)
+      self.plotData[1].append(rate)
+
 
     wx.Frame.__init__(self, None, -1, 'Rate configuration')
     self.curvePanel = wx.Panel(self)
@@ -45,7 +37,7 @@ class RateFrame(wx.Frame):
     self.axes = self.figure.add_subplot(111)
     self.axes.set_title('Rates')
     self.figure.autofmt_xdate()
-    self.curve = self.axes.plot(plotData[0], plotData[1], marker='o', picker=5)[0]
+    self.curve = self.axes.plot(self.plotData[0], self.plotData[1], marker='o', picker=5)[0]
     self.axes.set_xlim(date(2014, 1, 1), date(2016, 12, 1))
     self.axes.set_ylim(0.0, 10.0)
     self.axes.set_yticks(range(0, 11))
@@ -65,15 +57,15 @@ class RateFrame(wx.Frame):
     self.canvas.mpl_connect('motion_notify_event', self.onmotion)
 
   def drawRates(self):
-    self.curve.set_xdata(plotData[0])
-    self.curve.set_ydata(plotData[1])
+    self.curve.set_xdata(self.plotData[0])
+    self.curve.set_ydata(self.plotData[1])
 
     if self.dragging:
       if self.annotation != None:
         self.annotation.remove()
       self.annotation = self.axes.annotate("{0}  -  {1}%".format(
-                          plotData[0][self.draggedIndex], plotData[1][self.draggedIndex]),
-                          xy=(plotData[0][self.draggedIndex], plotData[1][self.draggedIndex]))
+                          self.plotData[0][self.draggedIndex], self.plotData[1][self.draggedIndex]),
+                          xy=(self.plotData[0][self.draggedIndex], self.plotData[1][self.draggedIndex]))
 
     self.canvas.draw();
 
@@ -96,15 +88,15 @@ class RateFrame(wx.Frame):
   def onmotion(self, event):
     if self.dragging:
       withtime = num2date(event.xdata)
-      plotData[0][self.draggedIndex] = date(withtime.year, withtime.month, 1)
-      plotData[1][self.draggedIndex] = round(event.ydata*2+0.5)/2
-      while self.draggedIndex+1 < len(plotData[0]) and plotData[0][self.draggedIndex] > plotData[0][self.draggedIndex+1]:
-        plotData[0][self.draggedIndex], plotData[0][self.draggedIndex+1] = plotData[0][self.draggedIndex+1], plotData[0][self.draggedIndex]
-        plotData[1][self.draggedIndex], plotData[1][self.draggedIndex+1] = plotData[1][self.draggedIndex+1], plotData[1][self.draggedIndex]
+      self.plotData[0][self.draggedIndex] = date(withtime.year, withtime.month, 1)
+      self.plotData[1][self.draggedIndex] = round(event.ydata*2+0.5)/2
+      while self.draggedIndex+1 < len(self.plotData[0]) and self.plotData[0][self.draggedIndex] > self.plotData[0][self.draggedIndex+1]:
+        self.plotData[0][self.draggedIndex], self.plotData[0][self.draggedIndex+1] = self.plotData[0][self.draggedIndex+1], self.plotData[0][self.draggedIndex]
+        self.plotData[1][self.draggedIndex], self.plotData[1][self.draggedIndex+1] = self.plotData[1][self.draggedIndex+1], self.plotData[1][self.draggedIndex]
         self.draggedIndex += 1
-      while self.draggedIndex > 0 and plotData[0][self.draggedIndex] < plotData[0][self.draggedIndex-1]:
-        plotData[0][self.draggedIndex], plotData[0][self.draggedIndex-1] = plotData[0][self.draggedIndex-1], plotData[0][self.draggedIndex]
-        plotData[1][self.draggedIndex], plotData[1][self.draggedIndex-1] = plotData[1][self.draggedIndex-1], plotData[1][self.draggedIndex]
+      while self.draggedIndex > 0 and self.plotData[0][self.draggedIndex] < self.plotData[0][self.draggedIndex-1]:
+        self.plotData[0][self.draggedIndex], self.plotData[0][self.draggedIndex-1] = self.plotData[0][self.draggedIndex-1], self.plotData[0][self.draggedIndex]
+        self.plotData[1][self.draggedIndex], self.plotData[1][self.draggedIndex-1] = self.plotData[1][self.draggedIndex-1], self.plotData[1][self.draggedIndex]
         self.draggedIndex -= 1
       self.drawRates()
 
@@ -121,7 +113,19 @@ class RateFrame(wx.Frame):
 
 
 if __name__ == '__main__':
+  interestAtDate_data = [
+    (date(2014, 1, 1), 3.0),
+    (date(2014, 2, 1), 2.0),
+    (date(2014, 3, 1), 3.0),
+    (date(2014, 4, 1), 4.0),
+    (date(2014, 5, 1), 3.0),
+    (date(2015, 3, 1), 4.0),
+    (date(2015, 8, 1), 5.0),
+    (date(2016, 2, 1), 6.0),
+    (date(2016, 5, 1), 7.0)
+  ]
+
   app = wx.PySimpleApp()
-  app.frame = RateFrame();
+  app.frame = RateFrame(interestAtDate_data);
   app.frame.Show()
   app.MainLoop()
