d8a262d1f6b44acc89f499315d7be93f01f93c87
Martin Nilsson <ibbles@hotmail.com>
2014-03-13 18:10:35 +0100

    Added NumberSequences.NumberList class.

--- NumberSequences.py
+++ NumberSequences.py
@@ -1,3 +1,73 @@
+
+def getDate(dateNumber):
+  return dateNumber[0]
+
+def getNumber(dateNumber):
+  return dateNumber[1]
+
+
+
+class NumberList(object):
+  """A NumberList is a sorted list of date-number pairs."""
+
+  def __init__(self, list):
+    self.__list = sorted(list, key = lambda entry: entry[0])
+
+  def insert(self, dateNumber):
+    print("inserting {}, {}".format(type(dateNumber[0]), type(dateNumber[1])))
+    # Handle corner cases first so that the while-loop berlow becomes easier.
+    if self.getSize() == 0:
+      self.__list.append(dateNumber)
+      return
+    if self.getLastDate() < getDate(dateNumber):
+      self.__list.append(dateNumber)
+      return
+    if self.getFirstDate() > getDate(dateNumber):
+      self.__list.insert(0, dateNumber)
+      return
+    # We now know that the new dateNumber will go to a valid index of the list.
+    # \todo The list is sorted, so consider a binary search here if required.
+    index = 0
+    while self.getDate(index) < getDate(dateNumber):
+      index += 1
+    self.__list.insert(index, dateNumber)
+
+
+  def getPair(self, index):
+    return self.__list[index]
+
+  def getDate(self, index):
+    return getDate(self.__list[index])
+
+  def getFirstDate(self):
+    return getDate(self.__list[0])
+
+  def getLastDate(self):
+    return getDate(self.__list[self.getLastIndex()])
+
+  def getNumber(self, index):
+    return getNumber(self.__list[index])
+
+  def getSize(self):
+    return len(self.__list)
+
+  def getLastIndex(self):
+    if self.getSize() == 0:
+      return None
+    return self.getSize() - 1
+
+
+
+  def __gettitem__(self, index):
+    return self.getPair(index)
+
+  def __iter__(self):
+    return iter(self.__list)
+
+
+
+
+
 class NumberSequence(object):
   """A NumberSequence is an object that translates dates to floating point
 numbers. Subclasses provide various ways of doing this translations."""
@@ -27,7 +97,10 @@ class StepNumber(NumberSequence):
   def __init__(self, dateNumberList):
     """The dateNumberList should be a list of two-element tuples where the
     first element of each tuple is a date and the second element is a number."""
-    self.__dateNumberList = sorted(dateNumberList, key = lambda entry: entry[0])
+    if isinstance(dateNumberList, NumberList):
+      self.__dateNumberList = dateNumberList
+    else:
+      self.__dateNumberList = NumberList(sorted(dateNumberList, key = lambda entry: entry[0]))
 
   def getNumber(self, date):
     if len(self.__dateNumberList) == 0:
@@ -39,3 +112,21 @@ class StepNumber(NumberSequence):
       else:
         lastNumber = number
     return lastNumber
+
+
+
+class LinearInterpolation(NumberSequence):
+  """The LinearInterpolation NumberSequence contains a sequence of fixed numbers, each
+  number associated with a particular dates. Queries for intermediate dates will return
+  values linearly interpolated between the two neighboring values."""
+
+  def __init__(self, dateNumberList):
+    """The dateNumberList should be a list of two-element tuples wihere the
+    first element of each tuple is a date and the second element is a number."""
+    self.__dateNumberList = sorted(dateNumberList, key = lambda entry: entry[0])
+
+    def getNumber(self, date):
+      if len(self.__dateNumberList) == 0:
+        return None
+      if getDate(self.dateNumberList[0]) > date:
+        return getValue(self.dateNumberList[0])
