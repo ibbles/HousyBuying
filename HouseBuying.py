25e761e9eb91ceb53d848680a94c7224904facec
Martin Nilsson <ibbles@hotmail.com>
2014-03-14 23:11:40 +0100

    Example application containing a CurveFrame.
    Also added debugger hook.

--- HouseBuying.py
+++ HouseBuying.py
@@ -3,9 +3,32 @@
 from Accounts import Account
 from Interest import Interest
 from Interest import InterestRate
-from NumberSequences import StepNumber
+from NumberSequences import NumberList
+from CurveFrame import CurveFrame
 
 import datetime
+
+import wx
+
+
+# code snippet, to be included in 'sitecustomize.py'
+import sys
+
+def info(type, value, tb):
+   if hasattr(sys, 'ps1') or not sys.stderr.isatty():
+      # we are in interactive mode or we don't have a tty-like
+      # device, so we call the default hook
+      sys.__excepthook__(type, value, tb)
+   else:
+      import traceback, pdb
+      # we are NOT in interactive mode, print the exception...
+      traceback.print_exception(type, value, tb)
+      print
+      # ...then start the debugger in post-mortem mode.
+      pdb.pm()
+
+sys.excepthook = info
+
 
 interestAtDate_data = [
   (datetime.date(2014, 1, 1), 1.0),
@@ -18,18 +41,8 @@ interestAtDate_data = [
   (datetime.date(2016, 2, 1), 1.0),
   (datetime.date(2016, 5, 1), 1.0)
 ]
-interestAtDate = StepNumber(interestAtDate_data)
-interestRate = InterestRate(interestAtDate)
-interest = Interest(interestRate);
 
-payingAccount = Account()
-payingAccount = payingAccount.deposit(20)
-print("At start of time: Balance = {0:.2f}, interest={1:.2f}.".format(payingAccount.getBalance(), payingAccount.getStoredInterest()))
-for year in range(2014, 2019):
-  for month in range(1,13):
-    date = datetime.date(year, month, 1)
-    payingAccount = payingAccount.applyInterest(interest, date, 1.0/12.0)
-    print("At end of month {0}-{1:02d}: Balance = {2:.2f}, interest={3:.2f}.".format(year, month, payingAccount.getBalance(), payingAccount.getStoredInterest()))
-  payingAccount = payingAccount.collectInterest()
-  print("At end of year {0}: Balance = {1:.2f}, interest={2:.2f}.".format(year, payingAccount.getBalance(), payingAccount.getStoredInterest()))
-print("At end of time: Balance = {0:.2f}, interest={1:.2f}.".format(payingAccount.getBalance(), payingAccount.getStoredInterest()))
+app = wx.PySimpleApp()
+app.frame = CurveFrame(NumberList(interestAtDate_data), 'Interest rate')
+app.frame.Show()
+app.MainLoop()
