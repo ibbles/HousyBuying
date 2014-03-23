5c02a5da1635f8a6cfb873e4e12b42fb0daa432c
Martin Nilsson <ibbles@hotmail.com>
2014-03-23 11:45:43 +0100

    Accounds now have names and updates their own storedInterest when applyInterest is called.

--- Accounts.py
+++ Accounts.py
@@ -1,22 +1,35 @@
 
+from Interest import Interest
+from NumberSequences import DateNumberList
+from NumberSequences import LinearInterpolation
+
 class Account(tuple):
-  def __init__(self, balance=0.0, storedInterest=0.0, interest=None):
+  def __init__(self, name, interestRate=None, balance=0.0, storedInterest=0.0):
     if balance == None:
       balance = 0.0
     if storedInterest == None:
       storedInterest = 0.0
     if interestRate == None:
-      interestRate = Interest(InterestRate(LinearInterpolation(DateNumberList([]))))
+      dateToInterestTable = DateNumberList([])
+      linearInterpolation = LinearInterpolation(dateToInterestTable)
+      interestRate = Interest(linearInterpolation)
+    self.__name = name
     self.__balance = balance
     self.__storedInterest = storedInterest
     self.__interestRate = interestRate
 
+
+  def getName(self):
+    return self.__name
 
   def getBalance(self):
     return self.__balance
 
   def getStoredInterest(self):
     return self.__storedInterest
+
+  def getDateInterestList(self):
+    return self.__interestRate
 
   def deposit(self, amount):
     self.__balance += amount
@@ -25,8 +38,7 @@ class Account(tuple):
     self.__balance -= amount
 
   def applyInterest(self, date):
-
-    return Account(self.__balance, self.__storedInterest+interest.calculateInterest(self.__balance, date, timeFraction))
+    self.__storedInterest += interest.calculateInterest(self.__balance, date)
 
   def collectInterest(self):
     self.__balance += self.__storedInterest
